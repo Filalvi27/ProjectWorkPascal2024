65422943d8fa6c73a99d9e8a1b3a7d56784b4024
XF4rry <christian.pastori.stud@ispascalcomandini.it>
2024-06-06 14:32:49 +0200

    finito header, footer, side-bar. ritocchi qua e la

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { FooterComponent } from './components/footer/footer.component';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { HomeComponent } from './components/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,FooterComponent,CommonModule, AppComponent, HeaderComponent, FooterComponent, SideBarComponent],
+  imports: [HomeComponent,RouterOutlet,FooterComponent,CommonModule, AppComponent, HeaderComponent, FooterComponent, SideBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
