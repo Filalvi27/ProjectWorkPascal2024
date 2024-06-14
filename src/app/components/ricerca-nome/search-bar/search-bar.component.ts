0288f1dba30ff50642b70a53b075a0d30bd19916
LeonelliDavide <davide.leonelli.stud@ispascalcomandini.it>
2024-06-14 14:31:36 +0200

    finito

--- src/app/components/ricerca-nome/search-bar/search-bar.component.ts
+++ src/app/components/ricerca-nome/search-bar/search-bar.component.ts
@@ -3,18 +3,18 @@ import { RouterModule } from '@angular/router';
 import { RicercaNomeComponent } from '../ricerca-nome.component';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import  {FormsModule, ReactiveFormsModule} from '@angular/forms'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { ProdottiService } from '../../../services/prodotti.service';
 
 @Component({
   selector: 'app-search-bar',
   standalone: true,
-  imports: [RouterModule, CommonModule,FormsModule, ReactiveFormsModule],
+  imports: [RouterModule, CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './search-bar.component.html',
   styleUrl: './search-bar.component.css',
 })
 export class SearchBarComponent {
-  constructor(public ricercaNomeComponent: RicercaNomeComponent, private router:Router, private prodottiService:ProdottiService) {}
+  constructor(public ricercaNomeComponent: RicercaNomeComponent, private router: Router, private prodottiService: ProdottiService) { }
 
   onKeydown(event: KeyboardEvent, form: HTMLFormElement) {
     // Verifica se il tasto premuto è Enter
@@ -27,10 +27,10 @@ export class SearchBarComponent {
       if (searchText) {
         console.log(searchText);
         //this.ricercaNomeComponent.onSubmit(form);
-        this.router.navigate(['/ricerca'],{queryParams:{searchText:searchText}})
+        this.router.navigate(['/ricerca'], { queryParams: { searchText: searchText } })
 
       }
-      
+
       // this.navigateAndClose('/ricerca');
       // this.ricercaNomeComponent.onSubmit(form);
     }
@@ -44,7 +44,7 @@ export class SearchBarComponent {
     if (searchText) {
       console.log(searchText);
       //this.ricercaNomeComponent.onSubmit(form);
-      this.router.navigate(['/ricerca'],{queryParams:{searchText:searchText}})
+      this.router.navigate(['/ricerca'], { queryParams: { searchText: searchText } })
       this.prodottiService.setCan(true)
     }
 
